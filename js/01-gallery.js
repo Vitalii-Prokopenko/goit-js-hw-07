ad21cc3ac0740ed3c0a0706fe4ce22e47a8969a9
Vitalii-Prokopenko <doc8259@gmail.com>
2023-02-28 22:55:21 +0200

    gallery #1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const imageGallery = document.querySelector(".gallery");
+// imageGallery.classList.add("list");
+let markup = "";
+
+galleryItems.forEach((galleryItem) => {
+  //   image.width = 1260;
+  //   image.height = 750;
+
+  let galleryElement = document.createElement("div");
+  let galleryLink = document.createElement("a");
+  let galleryImage = document.createElement("img");
+
+  galleryElement.classList.add("gallery__item");
+  galleryLink.classList.add("gallery__link");
+  galleryImage.classList.add("gallery__image");
+
+  galleryLink.setAttribute("href", galleryItem.original);
+  galleryImage.setAttribute("src", galleryItem.preview);
+  galleryImage.setAttribute("data-source", galleryItem.original);
+  galleryImage.setAttribute("alt", galleryItem.description);
+
+  console.log(galleryElement);
+  console.log(galleryLink);
+  console.log(galleryImage);
+
+  markup += `<${galleryElement.localName}><${galleryLink.localName} href="${
+    galleryLink.href
+  }"><${galleryImage.localName} src="${galleryImage.src}" alt="${
+    galleryImage.alt
+  }" data-source="${galleryImage.dataset.source}"/></${
+    galleryLink.localName
+  }></${galleryElement.localName}>`;
+});
+imageGallery.insertAdjacentHTML("afterbegin", markup);
+
+// console.log(galleryItems);
